97790f4507028c4105eb4626e49cfc07a5a46004
slimshady7111 <iniorg472@gmail.com>
2025-06-24 01:55:58 +0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#111", color: "#fff" }}>
-      <h1>Selamat datang di Baikhati AI</h1>
-    </div>
-  );
-}
