7d0edf60529c74a83572f2b3e18cc095b85c3479
slimshady7111 <iniorg472@gmail.com>
2025-06-25 20:10:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#111",
+      color: "#fff"
+    }}>
+      <h1>Selamat datang di Baikhati AI</h1>
+    </div>
+  );
+}
